ba64947fbaadff8ca585ac017c8f0e095243147d
kaps2607 <kapillund29@gmail.com>
2025-06-04 15:24:49 +0530

    commit

--- src/Core/DataMgmtModule.Domain/Entities/Properties.cs
+++ src/Core/DataMgmtModule.Domain/Entities/Properties.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace DataMgmtModule.Domain.Entities
 {
@@ -13,32 +9,31 @@ namespace DataMgmtModule.Domain.Entities
         [Key]
         public int Id { get; set; }
         public int TestId { get; set; }
-        public bool Sustainable { get; set; } = false;
-        public bool FlameRetardant { get; set; } = false;
-        public bool HeatStabilized130 { get; set; } = false;
-        public bool HeatStabilized160 { get; set; } = false;
-        public bool HeatStabilized230 { get; set; } = false;
-        public bool HydrolysisStabilized { get; set; } = false;
-        public bool LaserTransparent { get; set; } = false;
-        public bool LaserMarkable { get; set; } = false;
-        public bool LowWarpage { get; set; } = false;
-        public bool ReducedDensity { get; set; } = false;
-        public bool ReducedMoisture { get; set; } = false;
-        public bool ElectricallyNeutral { get; set; } = false;
-        public bool UVStabilized { get; set; } = false;
-        public bool SurfaceModified { get; set; } = false;
-        public bool AdhesionModified { get; set; } = false;
-        public bool TribologicalModified { get; set; } = false;
-        public bool EasyFlow { get; set; } = false;
-        public bool Nucleated { get; set; } = false;
-        public bool ProcessImproved { get; set; } = false;
-        public bool FluidInjection { get; set; } = false;
-        public bool RecycledContent { get; set; } = false;
-        public bool AdditiveManufacturing { get; set; } = false;
-        public bool IsDelete { get; set; } = false;
+
+        public bool? Sustainable { get; set; }
+        public bool? FlameRetardant { get; set; }
+        public bool? HeatStabilized130 { get; set; }
+        public bool? HeatStabilized160 { get; set; }
+        public bool? HeatStabilized230 { get; set; }
+        public bool? HydrolysisStabilized { get; set; }
+        public bool? LaserTransparent { get; set; }
+        public bool? LaserMarkable { get; set; }
+        public bool? LowWarpage { get; set; }
+        public bool? ReducedDensity { get; set; }
+        public bool? ReducedMoisture { get; set; }
+        public bool? ElectricallyNeutral { get; set; }
+        public bool? UVStabilized { get; set; }
+        public bool? SurfaceModified { get; set; }
+        public bool? AdhesionModified { get; set; }
+        public bool? TribologicalModified { get; set; }
+        public bool? EasyFlow { get; set; }
+        public bool? Nucleated { get; set; }
+        public bool? ProcessImproved { get; set; }
+        public bool? FluidInjection { get; set; }
+        public bool? RecycledContent { get; set; }
+        public bool? AdditiveManufacturing { get; set; }
+        public bool? IsDelete { get; set; }
 
         public Test Test { get; set; }
-
-
     }
 }
